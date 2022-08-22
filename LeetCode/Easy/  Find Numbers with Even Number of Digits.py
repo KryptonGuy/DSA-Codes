c04a92efbc9d8fba9048c7c409df19d938713c01
KryptonGuy <yash.sant.orignal@gmail.com>
2022-08-23 00:25:08 +0530

    minor changes

--- LeetCode/Easy/  Find Numbers with Even Number of Digits.py
+++ LeetCode/Easy/  Find Numbers with Even Number of Digits.py
@@ -4,7 +4,7 @@ class Solution:
     def findNumbers(self, nums: List[int]) -> int:
         even_digit = 0
         for num in nums:
-            digit = int(math.log10(num)) + 1
+            digit = int(log10(num)) + 1
             if digit%2 == 0:
                 even_digit += 1
         return even_digit
---
+++ LeetCode/Easy/Odd Even Linked List.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        if not head or not head.next:
+            return head
+ 
+        odd, startenv, even = head, head.next, head.next
+                
+        while odd.next and even.next:
+            odd.next = even.next
+            odd = even.next
+            
+            even.next = odd.next
+            even = odd.next
+            
+        odd.next = startenv
+        
+        return head
